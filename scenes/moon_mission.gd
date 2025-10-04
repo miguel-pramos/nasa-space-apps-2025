a2d5b421e159f10d544974cd556fcb263aede473
Asuka27 <hidalgo.anabeatriz@gmail.com>
2025-10-04 14:30:07 -0300

    Creating the scene missions

---
+++ scenes/moon_mission.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_start_mission_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
+
+
+func _on_start_mission_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
---
+++ select_place.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_moon_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/moon_mission.tscn")
+
+
+func _on_start_mart_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mart_missions.tscn")
+
+
+func _on_start_orbt_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/orbt_mission.tscn")
