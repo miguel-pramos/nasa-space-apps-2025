06c15150df8bdcd2f2bcca2e202e4a8b1a510c35
Asuka27 <hidalgo.anabeatriz@gmail.com>
2025-10-05 14:49:01 -0300

    Animations and setings

--- scenes/menu_start.gd
+++ scenes/menu_start.gd
@@ -2,11 +2,11 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/select_place.tscn")
+	get_tree().change_scene_to_file("res://scenes/scena_intro.tscn")
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/setings.tscn")
 
 
 func _on_exit_pressed() -> void:
